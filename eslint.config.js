1386b147fdd409808452c9da6f0bc52f8f5a6222
tbressel <tbressel.dev@gmail.com>
2025-05-26 16:07:38 +0200

    CICD: fix eslint on unit tests repertory

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,24 @@
-import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
+const globalsNode = globals.node;
+const globalsBrowser = globals.browser;
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"],
+  globalIgnores(["__tests__/**"]),
+
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globalsNode,
+        ...globalsBrowser,
+      },
+    },
     rules: {
-      //"no-console": "warn",
+    //  "no-console": "warn",
       "no-unused-vars": "error", // erreur pour une variable non utilisée
       "eqeqeq": ["error", "always"], // erreur pour une utilisation de comparaison autre que !== ou ===
       "no-undef": "error", // erreur pour l'utilisation d'une variable non définie.
@@ -18,13 +27,32 @@ export default defineConfig([
       "max-params": ["warn", 4], // limiter le nombre de paramètre dasn les focntion 
       "complexity": ["warn", 2], // empêche trop de condition imbriquée DANS UNE FONCTION
     },
-
-  },
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    languageOptions: { globals: globals.browser }
   },
 ]);
 
+// import { defineConfig, globalIgnores } from "eslint/config";
 
-export const ignores = ["__test__/**"];
+// export default defineConfig([
+//   // 👇 Ignore global pour __tests__
+//   globalIgnores(["__tests__/**"]),
+
+//   {
+//     files: ["**/*.{js,mjs,cjs}"],
+//     languageOptions: {
+//       ecmaVersion: "latest",
+//       sourceType: "module",
+//     },
+//     rules: {
+//        rules: {
+//       //"no-console": "warn",
+//       "no-unused-vars": "error", // erreur pour une variable non utilisée
+//       "eqeqeq": ["error", "always"], // erreur pour une utilisation de comparaison autre que !== ou ===
+//       "no-undef": "error", // erreur pour l'utilisation d'une variable non définie.
+//       "prefer-const": "error",  // erreur quand une variable n'est pas ré assigné.
+//       "no-empty-function": "warn", // interdit les fonction vide
+//       "max-params": ["warn", 4], // limiter le nombre de paramètre dasn les focntion 
+//       "complexity": ["warn", 2], // empêche trop de condition imbriquée DANS UNE FONCTION
+//     },
+//     },
+//   },
+// ]);
