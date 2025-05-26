246b4daf5cfc6732e1fa8d1ea1e95074a12204d0
tbressel <tbressel.dev@gmail.com>
2025-05-26 13:45:10 +0200

    CICD: fix es lint checker

--- src/counter.js
+++ src/counter.js
@@ -8,33 +8,33 @@ export function setupCounter(element) {
   setCounter(0)
 }
 
-let number1 = 12;
-let number2 = 456;
+// let number1 = 12;
+// let number2 = 456;
 
-if (number1 != number2) {
-  console.log("OK")
-}
+// if (number1 != number2) {
+//   console.log("OK")
+// }
 
 
-let text= 'cool';
+// let text= 'cool';
  
 
-function tooMuch(number1, number2, coucou, toto, raler) {
+// function tooMuch(number1, number2, coucou, toto, raler) {
 
-  if (number1) {
-    console.log("number exists")
+//   if (number1) {
+//     console.log("number exists")
   
-    if (number2) {
-      console.log('number2 exists')
+//     if (number2) {
+//       console.log('number2 exists')
   
-      if (number1 < number2) {
-        console.log("number 1 et inférieur à number 2")
-      }
-    }
-  }
-}
+//       if (number1 < number2) {
+//         console.log("number 1 et inférieur à number 2")
+//       }
+//     }
+//   }
+// }
 
 
-tooMuch();
+// tooMuch();
 
 
