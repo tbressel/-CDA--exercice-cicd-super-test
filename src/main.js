9d8e44a2ed757ed7ac9b9de57cadd48aff10c780
tbressel <tbressel.dev@gmail.com>
2025-05-26 11:35:02 +0200

    CORE: first deposit

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ document.querySelector('#app').innerHTML = `
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript log" />
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
