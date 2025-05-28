26358d438e1f3b509d86e23d244d314e0f101a4d
tbressel <tbressel.dev@gmail.com>
2025-05-28 10:21:01 +0200

    CICD: add build of the application

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,16 @@ import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globals: true, // Active les variables globales pour les tests
-    environment: 'node', // Définit l'environnement Node.js pour les tests
-    include: ['**/__tests__/**/*.test.js'], // Inclut les fichiers de test
-    exclude: [...configDefaults.exclude, 'node_modules/**'], // Exclut les fichiers inutiles
+    globals: true, 
+    environment: 'node', 
+    include: ['**/__tests__/**/*.test.js'], 
+    exclude: [...configDefaults.exclude, 'node_modules/**'], 
     coverage: {
-      enabled: true, // Active la collecte de couverture
-      reporter: ['text', 'lcov'], // Formats de rapport de couverture
-      reportsDirectory: '__tests__/coverage', // Répertoire de sortie des rapports
+      enabled: true, 
+      reporter: ['text', 'lcov'], 
+      reportsDirectory: '__tests__/coverage', 
     },
     alias: {
-      // Ajout d'alias pour les chemins relatifs (équivalent de moduleNameMapper)
       '^(\\.{1,2}/.*)\\.js$': '$1',
     },
   },
